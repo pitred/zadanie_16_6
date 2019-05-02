e2073b6273d1ab05afcf53c4f67fa4c7bf5be8cd
Piotr Nowinski <pitred007git@gmail.com>
2019-05-02 11:38:19 +0200

    Add prefix var to JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 // function responsible for getting quote
 function getQuote() {
-    fetch(quoteUrl, {
+    fetch(prefix + quoteUrl, {
             cache: "no-store"
         })
         .then(function (resp) {
